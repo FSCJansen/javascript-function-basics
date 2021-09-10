3088e7c9e304e4269acae74c96ed8d964d2f38c0
Frans Jansen <info@fransjansen.eu>
2021-09-10 13:51:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let orderedPizzas = 2 ;
+let orderedSushis = 3 ;
+
+const pizzaMaking = function(){
+console.log("Kneed het deeg");
+console.log("Maak een platte ronde schijf met een diameter van 25cm (de bodem)");
+console.log("Bestrijk de bodem met tomatensaus");
+console.log("verdeel gelijkmatig plakjes salami, stukjes champignons over de tomatensaus");
+console.log("Bestrooi het geheel met gerspte kaas");
+console.log("Bak de pizza 13 minuten in de over op 200 graden");
+};
+
+const sushiMaking = function(){
+    console.log("Kook de rijst 15 minuten");
+    console.log("Stap 2");
+    console.log("Stap 3");
+    console.log("Stap 4");
+    console.log("Snij de rol in gelijke delen");
+    };
+
+console.log("orderedPizzas * pizzaMaking();");
+console.log("orderedSushis * sushiMaking();");
+
+
